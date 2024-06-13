8df710577285067255fa4cc625de3de48baab410
phil <125900697+philiptitus@users.noreply.github.com>
2024-06-13 11:12:00 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='my_django_project',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'manage.py = manage:main',
+        ],
+    },
+)
